6dff4833b9b4b4e92fbf5699abad2d8a0f1ba400
Deepika Bangarh <deepikabangarh@yahoo.com>
2023-08-01 14:28:01 +0100

    routes

--- app-backend/app/controllers/transactions_controller.rb
+++ app-backend/app/controllers/transactions_controller.rb
@@ -1,2 +1,26 @@
 class TransactionsController < ApplicationController
+
+  def index
+    @transactions = Transaction.order(created_at: :desc)
+    # render json: { transactions: @transactions }
+  end
+
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def new
+    @transaction = Transaction.new
+   end
+
+   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def transaction_params
+      params.require(:transaction).permit(:amount, :account_id)
+    end
 end
--- app-backend/config/routes.rb
+++ app-backend/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :accounts
+  resources :transactions
   get '/ping', to: 'ping#show', format: :json, as: :ping
 end
